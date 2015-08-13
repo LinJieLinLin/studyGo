415ddfb15f2be9462bb6ba720e22a8a1006e51dd
LinJieLinLin <993353454@qq.com>
2015-08-13 14:59:03 +0800

    add length

--- one/test_range.go
+++ one/test_range.go
@@ -7,25 +7,25 @@ import (
 
 func main() {
 	t1 := time.Now().UnixNano()
-	len := 1000000
+	l := 1000000
 	s := []string{}
-	for i := 0; i < len; i++ {
+	for i := 0; i < l; i++ {
 		s = append(s, `纳秒，时间单位。一秒的10亿分之一，即等于10的负9次方秒。常用作内存读写速度的单位，其前面数字越小则表示速度越快。
 					纳秒也是计算机中的一个专业术语，是用来计算CPU及各个硬件所运行的速度的运行单位。
 					1纳秒=0.000000001秒=10^(-9)秒`)
 	}
 	fmt.Println("1", float64((time.Now().UnixNano()-t1))/float64(int64(time.Second)), "秒\n")
-	fmt.Println("now len:",len(s))
+	fmt.Println("now length:",len(s))
 	one(s)
 	two(s)
-	fmt.Println("len 在 1000000 以内，_,o:=range 比i,_:=range 好")
-	for i := 0; i < len*9; i++ {
+	fmt.Println("length 在 1000000 以下，_,o:=range 比i,_:=range 好")
+	for i := 0; i < l*9; i++ {
 		s = append(s, `纳秒，时间单位。一秒的10亿分之一，即等于10的负9次方秒。常用作内存读写速度的单位，其前面数字越小则表示速度越快。
 					纳秒也是计算机中的一个专业术语，是用来计算CPU及各个硬件所运行的速度的运行单位。
 					1纳秒=0.000000001秒=10^(-9)秒`)
 	}
 	fmt.Println("1", float64((time.Now().UnixNano()-t1))/float64(int64(time.Second)), "秒\n")
-	fmt.Println("now len:",len(s))
+	fmt.Println("now length:",len(s))
 	one(s)
 	two(s)
 }
